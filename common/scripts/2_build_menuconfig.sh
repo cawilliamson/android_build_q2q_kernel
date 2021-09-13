d85ccf127421b65d120731a4661220161f39883a
Christopher A. Williamson <home@chrisaw.com>
2021-09-13 09:37:37 +0100

    Move build process to this tree and out of kernel

--- common/scripts/2_build_menuconfig.sh
+++ common/scripts/2_build_menuconfig.sh
@@ -3,8 +3,35 @@
 # change working directory
 cd /usr/src/kernel
 
-# execute actual build
-bash build_menuconfig.sh
+ARCH=arm64
+PROJECT_NAME=q2q
+export ARCH PROJECT_NAME
+
+BUILD_CROSS_COMPILE="/opt/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+CLANG_TRIPLE="aarch64-linux-gnu-"
+KERNEL_LLVM_BIN="/opt/qcom-clang/bin/clang"
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
+mkdir -p out/
+
+make -C $(pwd) \
+  O=$(pwd)/out \
+  $KERNEL_MAKE_ENV \
+  CLANG_TRIPLE=$CLANG_TRIPLE \
+  CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
+  CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+  REAL_CC=$KERNEL_LLVM_BIN \
+  vendor/q2q_eur_openx_defconfig
+
+make -C $(pwd) \
+  O=$(pwd)/out \
+  $KERNEL_MAKE_ENV \
+  CLANG_TRIPLE=$CLANG_TRIPLE \
+  CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
+  CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+  REAL_CC=$KERNEL_LLVM_BIN \
+  CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
+  menuconfig
 
 # remove previous defconfig
 rm -f /out/defconfig
--- common/scripts/3_build_zip.sh
+++ common/scripts/3_build_zip.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+ARCH=arm64
+PROJECT_NAME=q2q
+export ARCH PROJECT_NAME
+
+BUILD_CROSS_COMPILE="/opt/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+CLANG_TRIPLE="aarch64-linux-gnu-"
+KERNEL_LLVM_BIN="/opt/qcom-clang/bin/clang"
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
 # fetch anykernel3 sources
 git clone \
   https://github.com/osm0sis/AnyKernel3.git \
   /usr/src/anykernel3
 
-# remove example modules directory structure
+# create modules directory structure
 rm -rfv /usr/src/anykernel3/modules/*
+mkdir -p /usr/src/anykernel3/modules/vendor/lib/modules
 
 # create config
 cp -fv /common/configs/anykernel.sh /usr/src/anykernel3/anykernel.sh
@@ -14,11 +24,18 @@ cp -fv /common/configs/anykernel.sh /usr/src/anykernel3/anykernel.sh
 # gzip kernel image (and copy at the same time)
 gzip -9 -c /usr/src/kernel/out/arch/arm64/boot/Image > /usr/src/anykernel3/Image.gz
 
-# find + copy modules to zip and generate module.load list
-mkdir -p /usr/src/anykernel3/modules/system/vendor/lib/modules/
-find /usr/src/kernel/out -type f -name '*.ko' -exec \
-  cp -v "{}" "/usr/src/anykernel3/modules/system/vendor/lib/modules/" \;
-ls -1 /usr/src/anykernel3/modules/system/vendor/lib/modules/*.ko | sort | xargs -n1 basename > /usr/src/anykernel3/modules/system/vendor/lib/modules/modules.load
+# Install modules to anykernel3 modules/ dir
+pushd /usr/src/kernel
+  make -C $(pwd) \
+  O=$(pwd)/out \
+  $KERNEL_MAKE_ENV \
+  CLANG_TRIPLE=$CLANG_TRIPLE \
+  CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
+  CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+  INSTALL_MOD_PATH=/usr/src/anykernel3/modules/vendor \
+  REAL_CC=$KERNEL_LLVM_BIN \
+  modules_install
+popd
 
 # cleanup previous zips
 rm -f /out/*.zip
